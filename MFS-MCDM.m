6e53cde430c1805b793d3f3d3ab47c4f3c21556d
amin1373 <59017215+amin1373@users.noreply.github.com>
2020-12-10 01:50:07 +0330

    Update MFS-MCDM.m

--- MFS-MCDM.m
+++ MFS-MCDM.m
@@ -1,6 +1,9 @@
 
 % MFS-MCDM: Multi-label feature selection using multi-criteria decision making-2020
 
+function S1= MFS_MCDM(dataTrain,dataTrainLabels)
+
+[~, feature_num] = size( dataTrain);
 Lambda = 10;
 D = pinv(dataTrain'*dataTrain + Lambda*eye(feature_num)) * dataTrain'*dataTrainLabels;
 for t = 1:num_class
@@ -9,3 +12,4 @@ end
 W = w./sum(w);
 rCloseness = Topsis(D,ones(1,feature_num),W);
 [SW S1] = sort(-rCloseness);
+end
